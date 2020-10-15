c55d975f914671ed3ca4d9df800eaaf8dc9249c8
anhhuybkcntt01 <anhhuybkcntt01@gmail.com>
2020-10-15 09:03:56 +0700

    Create Model -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('products',\App\Http\Controllers\ProductController::class);
+
+Route::group(['prefix'=>'products'],function (){
+    Route::apiResource('/{product}/reviews',\App\Http\Controllers\ReviewController::class);
+});
